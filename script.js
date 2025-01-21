ef2e4f20201ed271a6971cbbc7506b178c21c9d1
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-01-21 13:47:20 +0530

    add gsap

---
+++ script.js
@@ -0,0 +1,36 @@
+gsap.from('.logo',{
+    y:-20,
+    duration: 1,
+    delay:0.2,
+})
+gsap.from('header button',{
+    y:-20,
+    duration: 1,
+    delay:0.5,
+})
+
+var tl=gsap.timeline()
+tl.from('.herotext',{
+    x:-900,
+    duration: 0.5,
+    delay:0.2,
+})
+
+tl.from('.heroImage',{
+    x:900,
+    duration: 0.5,
+    delay:0.2,
+})
+
+tl.from('.mechanical',{
+    y:-5000,
+    duration: 0.3,
+    delay:0.2,
+})
+
+tl.from('.feature',{
+    x:-5000,
+    duration: 1,
+    delay:0.1,
+    stagger:0.1,
+})
